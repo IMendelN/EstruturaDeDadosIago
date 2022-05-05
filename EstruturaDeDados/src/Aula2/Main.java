055daff4c96dfa0aef7f4529a9722de98250a428
Iago Mendes Nogueira <iagonogueira.aluno@unipampa.edu.br>
2022-05-05 14:37:18 -0300

    Ponto Cartesiano

---
+++ EstruturaDeDados/src/Aula2/Main.java
@@ -0,0 +1,12 @@
+package Aula2;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		PontoCartesiano a = new PontoCartesiano(1.2, 2.5, 3, 6);
+		a.distancia();
+		System.out.println(a.distancia());
+	}
+
+}
