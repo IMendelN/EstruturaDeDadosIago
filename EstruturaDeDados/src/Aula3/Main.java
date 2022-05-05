09237510c70c89be62cb4a690c8d433687797eaf
Iago Mendes Nogueira <iagonogueira.aluno@unipampa.edu.br>
2022-05-05 18:53:55 -0300

    Ponto cartesiano

---
+++ EstruturaDeDados/src/Aula3/Main.java
@@ -0,0 +1,13 @@
+package Aula3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AlunoVetor vetAluno = new AlunoVetor(" ", 10, 21);
+		vetAluno.nota(10);
+		vetAluno.nome("Iago");
+		vetAluno.idade(21);
+
+	}
+
+}
