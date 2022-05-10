d53e755aca2abaabc035ca881e241cd81bf248ee
Iago Mendes Nogueira <iagonogueira.aluno@unipampa.edu.br>
2022-05-09 21:21:42 -0300

    Correção Interface

--- EstruturaDeDados/src/Aula4/Ivetor.java
+++ EstruturaDeDados/src/Aula4/Ivetor.java
@@ -1,29 +1,31 @@
 package Aula4;
 
-public class Ivetor {
+public class Ivetor implements IVetorI {
 
 	private int val = 0;
 	private Aluno alunos[];
-	
+
 	public Ivetor(int tam) {
-		this.alunos = new Aluno[tam]; 
+		this.alunos = new Aluno[tam];
 	}
-	
+
 	public void adiciona(Aluno aluno) {
-		if(!cheio(aluno)) {
+		if (!cheio(aluno)) {
 			this.alunos[val] = aluno;
-			this.val++;	
-		}else {
+			this.val++;
+		} else {
 			garanteEspaco(aluno);
 			System.out.println("!Vetor Cheio!");
 		}
-		
+
 	}
-	
-	public int tamanho() {return val;}
-	
-	public boolean contem(Aluno aluno){
-		for(int i = 0; i < val; i++) {
+
+	public int tamanho() {
+		return val;
+	}
+
+	public boolean contem(Aluno aluno) {
+		for (int i = 0; i < val; i++) {
 			if (alunos[i] != null) {
 				if (this.alunos[i].getNome().equals(aluno.getNome())) {
 					return true;
@@ -35,20 +37,24 @@ public class Ivetor {
 	}
 
 	public boolean cheio(Aluno aluno) {
-		if(val == alunos.length) {
+		if (val == alunos.length) {
 			return true;
 		}
-		
+
 		return false;
 	}
 
-	public void garanteEspaco(Aluno aluno) {
-		if (this.cheio(aluno)){
+	public boolean remove(Aluno aluno) {
+		if (this.cheio(aluno)) {
 			Aluno newVet[] = new Aluno[this.alunos.length * 2];
 			for (int i = 0; i < this.alunos.length; i++) {
 				newVet[i] = this.alunos[i];
+
 			}
 			this.alunos = newVet;
+			return true;
+		}
+		return false;
 	}
-}
+
 }
